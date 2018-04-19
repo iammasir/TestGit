8c492fb376aa45aed5fc6428d222ca6fe8dc359a
mazhuo <iammasir@163.com>
2018-04-19 15:34:15 +0800

    Initial commit

---
+++ app/src/main/java/com/iammasir/testgit/GitNote.java
@@ -0,0 +1,39 @@
+package com.iammasir.testgit;
+
+/**
+
+ 下载安装地址：https://git-scm.com/book/zh/v1/起步-安装-Git
+
+ 选择use git from git bash only
+
+
+ 1.配置
+ $ git config --global user.name "Your Name"
+ $ git config --global user.email "email@example.com"
+
+ 注意：
+ .将上方的Your Name换成你自己的名字，随意起都行。
+ .将上方的email@example.com换成你自己的邮箱。
+ .讲一下git config命令的--global参数，用了这个参数，表示你这台机器上所有的Git仓库都会使用这个配置，当然也可以对某个仓库指定不同的用户名和Email地址。
+
+ 2.打开AS设置，找到 Version Control --> Git在Path To Git Executable上输入Git的存放位置 （在安装位置，找到cmd文件夹下的git.exe）
+
+ 3.打开AS设置，找到 Version Control --> GitHub  设置账号和密码
+
+ 4.对于已存在项目，选择VCS-import into Version Control-create Git Repository(选择项目根目录)
+
+ 5.添加文件
+
+ a.选中项目的根目录，右键选中Git，再选中Add
+ b.选择底部Version Control 选择Unversioned Files，右键选择Git - add
+
+
+
+
+
+
+
+ */
+
+public class GitNote {
+}
